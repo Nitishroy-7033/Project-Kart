b14f5ad675279afd019cfeb3f73d4ead89215b9d
Nitish Kumar <79355266+MrNitishroy@users.noreply.github.com>
2025-03-22 01:15:37 +0530

    [USER-PROFILE] Show order history ui added

--- src/pages/profile/userProfile.js
+++ src/pages/profile/userProfile.js
@@ -1,22 +1,193 @@
 import { TbLogout2 } from "react-icons/tb";
 import "./style.css";
 
-import { Button, Col, Layout, Row } from "antd";
+import { Button, Col, Divider, Layout, Row } from "antd";
 import { MdOutlineAddChart } from "react-icons/md";
 import { useNavigate } from "react-router-dom";
+import { BiRupee } from "react-icons/bi";
 
 const UserProfile = () => {
-  const navigate  = useNavigate();
-  const handleLogoutClick = ()=>{
+  const navigate = useNavigate();
+  const handleLogoutClick = () => {
     navigate("/auth");
-     localStorage.removeItem("role");
-  }
+    localStorage.removeItem("role");
+  };
+  const products = [
+    {
+      id: 1,
+      name: "Product 1",
+      price: 100,
+      imageUrls: [
+        "https://d2v5dzhdg4zhx3.cloudfront.net/web-assets/images/storypages/primary/ProductShowcasesampleimages/JPEG/Product+Showcase-1.jpg",
+        "https://d2v5dzhdg4zhx3.cloudfront.net/web-assets/images/storypages/primary/ProductShowcasesampleimages/JPEG/Product+Showcase-1.jpg",
+        "https://d2v5dzhdg4zhx3.cloudfront.net/web-assets/images/storypages/primary/ProductShowcasesampleimages/JPEG/Product+Showcase-1.jpg",
+      ],
+      isTranding: true,
+      ratings: 4.5,
+      reviews: 100,
+      sortDescription: "This is a product",
+      isLiked: false,
+    },
+    {
+      id: 2,
+      name: "Product 2",
+      price: 200,
+      imageUrls: [
+        "https://d2v5dzhdg4zhx3.cloudfront.net/web-assets/images/storypages/primary/ProductShowcasesampleimages/JPEG/Product+Showcase-1.jpg",
+        "https://d2v5dzhdg4zhx3.cloudfront.net/web-assets/images/storypages/primary/ProductShowcasesampleimages/JPEG/Product+Showcase-1.jpg",
+      ],
+      isTranding: true,
+      ratings: 4.5,
+      reviews: 100,
+      sortDescription: "This is a product",
+      isLiked: false,
+    },
+    {
+      id: 3,
+      name: "Product 3",
+      price: 300,
+      imageUrls: [
+        "https://d2v5dzhdg4zhx3.cloudfront.net/web-assets/images/storypages/primary/ProductShowcasesampleimages/JPEG/Product+Showcase-1.jpg",
+        "https://d2v5dzhdg4zhx3.cloudfront.net/web-assets/images/storypages/primary/ProductShowcasesampleimages/JPEG/Product+Showcase-1.jpg",
+        "https://d2v5dzhdg4zhx3.cloudfront.net/web-assets/images/storypages/primary/ProductShowcasesampleimages/JPEG/Product+Showcase-1.jpg",
+        "https://d2v5dzhdg4zhx3.cloudfront.net/web-assets/images/storypages/primary/ProductShowcasesampleimages/JPEG/Product+Showcase-1.jpg",
+      ],
+      isTranding: true,
+      ratings: 4.5,
+      reviews: 100,
+      sortDescription: "This is a product",
+      isLiked: false,
+    },
+    {
+      id: 4,
+      name: "Product 4",
+      price: 400,
+      imageUrls: [
+        "https://d2v5dzhdg4zhx3.cloudfront.net/web-assets/images/storypages/primary/ProductShowcasesampleimages/JPEG/Product+Showcase-1.jpg",
+        "https://d2v5dzhdg4zhx3.cloudfront.net/web-assets/images/storypages/primary/ProductShowcasesampleimages/JPEG/Product+Showcase-1.jpg",
+        "https://d2v5dzhdg4zhx3.cloudfront.net/web-assets/images/storypages/primary/ProductShowcasesampleimages/JPEG/Product+Showcase-1.jpg",
+      ],
+      isTranding: true,
+      ratings: 4.5,
+      reviews: 100,
+      sortDescription: "This is a product",
+      isLiked: false,
+    },
+    {
+      id: 5,
+      name: "Product 5",
+      price: 500,
+      imageUrls: [
+        "https://th.bing.com/th/id/OIP.qFLJlBnH2xkl508yedA9pQHaFS?w=1600&h=1141&rs=1&pid=ImgDetMain",
+        "https://th.bing.com/th/id/OIP.qFLJlBnH2xkl508yedA9pQHaFS?w=1600&h=1141&rs=1&pid=ImgDetMain",
+        "https://mir-s3-cdn-cf.behance.net/project_modules/1400_opt_1/f8db9894200401.5e78f0e37d1e4.png",
+      ],
+      isTranding: true,
+      ratings: 4.5,
+      reviews: 100,
+      sortDescription: "This is a product",
+      isLiked: false,
+    },
+    {
+      id: 3,
+      name: "Product 3",
+      price: 300,
+      imageUrls: [
+        "https://d2v5dzhdg4zhx3.cloudfront.net/web-assets/images/storypages/primary/ProductShowcasesampleimages/JPEG/Product+Showcase-1.jpg",
+        "https://d2v5dzhdg4zhx3.cloudfront.net/web-assets/images/storypages/primary/ProductShowcasesampleimages/JPEG/Product+Showcase-1.jpg",
+        "https://d2v5dzhdg4zhx3.cloudfront.net/web-assets/images/storypages/primary/ProductShowcasesampleimages/JPEG/Product+Showcase-1.jpg",
+        "https://d2v5dzhdg4zhx3.cloudfront.net/web-assets/images/storypages/primary/ProductShowcasesampleimages/JPEG/Product+Showcase-1.jpg",
+      ],
+      isTranding: true,
+      ratings: 4.5,
+      reviews: 100,
+      sortDescription: "This is a product",
+      isLiked: false,
+    },
+    {
+      id: 4,
+      name: "Product 4",
+      price: 400,
+      imageUrls: [
+        "https://d2v5dzhdg4zhx3.cloudfront.net/web-assets/images/storypages/primary/ProductShowcasesampleimages/JPEG/Product+Showcase-1.jpg",
+        "https://d2v5dzhdg4zhx3.cloudfront.net/web-assets/images/storypages/primary/ProductShowcasesampleimages/JPEG/Product+Showcase-1.jpg",
+        "https://d2v5dzhdg4zhx3.cloudfront.net/web-assets/images/storypages/primary/ProductShowcasesampleimages/JPEG/Product+Showcase-1.jpg",
+      ],
+      isTranding: true,
+      ratings: 4.5,
+      reviews: 100,
+      sortDescription: "This is a product",
+      isLiked: false,
+    },
+    {
+      id: 5,
+      name: "Product 5",
+      price: 500,
+      imageUrls: [
+        "https://th.bing.com/th/id/OIP.qFLJlBnH2xkl508yedA9pQHaFS?w=1600&h=1141&rs=1&pid=ImgDetMain",
+        "https://th.bing.com/th/id/OIP.qFLJlBnH2xkl508yedA9pQHaFS?w=1600&h=1141&rs=1&pid=ImgDetMain",
+        "https://mir-s3-cdn-cf.behance.net/project_modules/1400_opt_1/f8db9894200401.5e78f0e37d1e4.png",
+      ],
+      isTranding: true,
+      ratings: 4.5,
+      reviews: 100,
+      sortDescription: "This is a product",
+      isLiked: false,
+    },
+    {
+      id: 3,
+      name: "Product 3",
+      price: 300,
+      imageUrls: [
+        "https://d2v5dzhdg4zhx3.cloudfront.net/web-assets/images/storypages/primary/ProductShowcasesampleimages/JPEG/Product+Showcase-1.jpg",
+        "https://d2v5dzhdg4zhx3.cloudfront.net/web-assets/images/storypages/primary/ProductShowcasesampleimages/JPEG/Product+Showcase-1.jpg",
+        "https://d2v5dzhdg4zhx3.cloudfront.net/web-assets/images/storypages/primary/ProductShowcasesampleimages/JPEG/Product+Showcase-1.jpg",
+        "https://d2v5dzhdg4zhx3.cloudfront.net/web-assets/images/storypages/primary/ProductShowcasesampleimages/JPEG/Product+Showcase-1.jpg",
+      ],
+      isTranding: true,
+      ratings: 4.5,
+      reviews: 100,
+      sortDescription: "This is a product",
+      isLiked: false,
+    },
+    {
+      id: 4,
+      name: "Product 4",
+      price: 400,
+      imageUrls: [
+        "https://d2v5dzhdg4zhx3.cloudfront.net/web-assets/images/storypages/primary/ProductShowcasesampleimages/JPEG/Product+Showcase-1.jpg",
+        "https://d2v5dzhdg4zhx3.cloudfront.net/web-assets/images/storypages/primary/ProductShowcasesampleimages/JPEG/Product+Showcase-1.jpg",
+        "https://d2v5dzhdg4zhx3.cloudfront.net/web-assets/images/storypages/primary/ProductShowcasesampleimages/JPEG/Product+Showcase-1.jpg",
+      ],
+      isTranding: true,
+      ratings: 4.5,
+      reviews: 100,
+      sortDescription: "This is a product",
+      isLiked: false,
+    },
+    {
+      id: 5,
+      name: "Product 5",
+      price: 0,
+      imageUrls: [
+        "https://th.bing.com/th/id/OIP.qFLJlBnH2xkl508yedA9pQHaFS?w=1600&h=1141&rs=1&pid=ImgDetMain",
+        "https://th.bing.com/th/id/OIP.qFLJlBnH2xkl508yedA9pQHaFS?w=1600&h=1141&rs=1&pid=ImgDetMain",
+        "https://mir-s3-cdn-cf.behance.net/project_modules/1400_opt_1/f8db9894200401.5e78f0e37d1e4.png",
+      ],
+      isTranding: true,
+      ratings: 4.5,
+      reviews: 100,
+      sortDescription: "This is a product",
+      isLiked: false,
+    },
+  ];
   return (
     <Layout
-    style={{
-        height:"100vh"
-    }}
-    className="body-container">
+      style={{
+        minHeight: "100vh",
+      }}
+      className="body-container"
+    >
       <br></br>
       <br></br>
       <br></br>
@@ -36,26 +207,61 @@ const UserProfile = () => {
             Nitish kumar
           </Row>
           <Row
-            style={{
-              fontSize: "18px",
-              fontWeight: "600",
-            }}
+            className="lable-heading"
           >
             Nitishkumar@gmail.com
           </Row>
           <br></br>
-          <Row style={{
-            gap:'10px'
-          }}>
-            <Button onClick={()=>{
-              handleLogoutClick()
-            }}><TbLogout2/> Logout</Button>
-            <Button><MdOutlineAddChart/>  Post a Ads</Button>
+          <Row
+            style={{
+              gap: "10px",
+            }}
+          >
+            <Button
+              onClick={() => {
+                handleLogoutClick();
+              }}
+            >
+              <TbLogout2 /> Logout
+            </Button>
+            <Button>
+              <MdOutlineAddChart /> Post a Ads
+            </Button>
           </Row>
         </Col>
       </Row>
-      <br></br>
-      this is profiel
+      <Divider />
+      <div className="lable-heading">Order History</div>
+      <Col>
+        {products.map((e) => (
+          <Row justify={"space-between"} className="order-item-box">
+            <Row style={{
+              gap:"10px"
+            }}>
+              <div className="order-item-img-container ">
+                <img src={e.imageUrls[0]} className="order-item-img" />
+              </div>
+              <Col>
+                <div className="body-text">{e.name}</div>
+                <div className="lable-text">{e.sortDescription}</div>
+              </Col>
+            </Row>
+            <Col>
+              <Row align={"middle"} className="body-text">
+                <BiRupee />
+                {e.price}
+              </Row>
+            </Col>
+            <Col>
+              <Row justify={"end"}  align={"middle"} className="body-text">
+               <div className="primary-circle"></div>
+                Delived
+              </Row>
+              <Row justify={"end"}>12-02-3023</Row>
+            </Col>
+          </Row>
+        ))}
+      </Col>
     </Layout>
   );
 };
